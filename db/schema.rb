b69e7ec9e4c788b05775e32c11cc8553a4b4a9b7
MuhammedMoustafa1 <muhammed.moustafa.ahmed@gmail.com>
2024-05-24 18:32:54 +0300

    make address not null in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_150725) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_153159) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_150725) do
     t.date "dob"
     t.string "email"
     t.string "phone_number"
-    t.string "address"
+    t.string "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
