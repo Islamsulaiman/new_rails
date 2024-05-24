ccc3d5a6ce5a13cb52771c7641f43eef1907238e
dohaseif2 <92125041+dohaseif2@users.noreply.github.com>
2024-05-24 13:14:35 +0000

    make post controller with methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @posts=Post.all
   end
+  def new 
+    @post=Post.new
+  end
+  def show 
+    @post =Post.find(params[:id]) 
+  end
+  def create 
+    @post =Post.new({title:params[:post][:title],content:params[:post][:content]})
+    if @post.save
+      redirect_to post_url(@post) ,notice: "creatded!"
+    else
+      render :new ,notice:"not Created",status:422
+  end 
 end
