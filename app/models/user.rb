d8ebfe5fb9e4bcb2d108a4c090150a2a860db2fa
TalaatEysa <talaateysa@outlook.com>
2024-05-25 04:26:37 +1100

    Add email uniqness validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
     validates :name, :DOB, :email, :phone_number, presence: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, uniqueness: true
 end
