70b9bdb4af16965ec31900f7f66e111e1e4042e3
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2024-05-24 18:19:45 +0300

    add the address column to users database and not null

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, :dob, :email, :phone_number, presence: true
+    validates :name, :dob, :email, :phone_number, :address, presence: true
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "is not a valid email address" }
     validates :email, uniqueness: true
 end
