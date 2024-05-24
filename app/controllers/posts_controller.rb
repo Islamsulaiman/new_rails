57a0d4cb08d20d055aa00a4de275ccfc333715f0
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-05-24 22:12:35 +0200

    add show  method in PostController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,9 @@ class PostsController < ApplicationController
       def new
         @post = Post.new
       end
+      def show
+        @post = Post.find(params[:id])
+      end
+      
     end
 end
