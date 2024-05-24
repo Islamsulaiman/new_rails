16d537f83231e765c2946ecf792926dbaf47ad97
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-05-24 22:34:37 +0200

    add validations on email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     validates :name, :dob, :email, :phone_number, presence: true
+
+    validates :email, format: { with: URI::Email::EMAIL_REGEXP, message: "is not a valid email address" }
+    validates :email, uniqueness: true
 end
