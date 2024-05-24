bd9ee7740440345d8da8afec04e45021a1ef4605
buthinakhaled <buthinakhaled397@gmail.com>
2024-05-25 01:58:37 +0300

    make crud

--- db/migrate/20240524224720_add_index_to_users_email.rb
+++ db/migrate/20240524224720_add_index_to_users_email.rb
@@ -1,4 +1,6 @@
-class AddIndexToUsersEmail < ActiveRecord::Migration[7.1]
+
+class AddIndexToUsersEmail < ActiveRecord::Migration[6.0]
   def change
+    add_index :users, :email, unique: true
   end
 end
