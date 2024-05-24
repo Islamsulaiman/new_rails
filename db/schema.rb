edf95f0ad058800804de5577807b7f55f59ec252
dohaseif2 <dohaseif2@gmail.com>
2024-05-24 23:06:24 +0300

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_121910) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_154608) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "DOB"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
