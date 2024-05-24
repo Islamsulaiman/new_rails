b3d7598a3285717ea8e73175c0254026308985d5
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-05-24 22:19:05 +0200

    add create post in postController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,16 @@ class PostsController < ApplicationController
       def show
         @post = Post.find(params[:id])
       end
-      
+
+      def create
+        # puts "++++++ params #{params}"
+        @post = Post.new({title: params[:post][:title] , content: params[:post][:content]})
+    
+        if @post.save
+          redirect_to post_url base/posts(@post), notice: "Created !"
+        else
+    
+        render :new, notice: "not Created !" , status: 422
+        end
     end
 end
