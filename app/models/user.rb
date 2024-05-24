d0bc1fd935bc822c23979fbcb18863a1e7a6ec8c
Mona-Ali-Mostafa98 <monaali980628@gmail.com>
2024-05-24 17:20:25 +0300

    validate presence on all the feilds

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true
+    validates :DOB, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :phone_number, presence: true
+    validates :email, uniqueness: true
+  end
+  
