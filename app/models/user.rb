2f44478647886826d331039f65a34325c8e4bb18
Nada Alnajdi <nada.abdo.alnajdi@gmail.com>
2024-05-25 02:42:39 +0300

    "make model and add address"

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     validates :DOB, presence: true
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
     validates :phone_number, presence: true
+    validates :address, presence: true
+
 end
