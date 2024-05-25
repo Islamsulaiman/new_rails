c53885f8564a556d8f57fcd1722f0577ae5d2e32
nyraareda <nyraareda3@gmail.com>
2024-05-25 03:48:06 +0300

    lab 3 nyraa

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,48 @@
 class PostsController < ApplicationController
-   def index
-    end
+  def index
+    @posts = Post.all
+  end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # @post = Post.new(post_params)
+    @post =Post.new({title:params[:post][:title],content:params[:post][:content]})
+    if @post.save
+      redirect_to post_url(@post), notice: 'Post was successfully created.'
+    else
+      render :new ,notice: "not created" ,status:422
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
-Rails.application.routes.draw do
+# Rails.application.routes.draw do
+#   get 'posts/index'
+#   get 'posts/show'
+#   get 'posts/new'
+#   get 'posts/create'
+#   get 'posts/edit'
+#   get 'posts/update'
+#   get 'posts/destroy'
+
+  Rails.application.routes.draw do
+    resources :posts
+    # Other routes can go here
+    delete '/posts/:id', to: 'posts#destroy', as: 'delete_post'
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
