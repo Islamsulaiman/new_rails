d8dd69ed4f98a4458c46b916c550617a45feb5ce
nyraareda <nyraareda3@gmail.com>
2024-05-25 04:13:19 +0300

    lab 3 b validate on email to be valid email structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :DOB, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
     validates :phone_number, presence: true
   end
   
