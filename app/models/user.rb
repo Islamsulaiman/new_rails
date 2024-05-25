0dd1c23d346a842c635286539b867210c63f4a0f
nyraareda <nyraareda3@gmail.com>
2024-05-25 04:25:58 +0300

    lab 3 c add column addressNot Null

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     validates :DOB, presence: true
     validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, uniqueness: true
     validates :phone_number, presence: true
+    validates :address, presence: true
   end
   
