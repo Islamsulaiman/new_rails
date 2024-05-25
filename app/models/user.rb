9da96f130cdfcf54cf09277c8b156c9967434876
nyraareda <nyraareda3@gmail.com>
2024-05-25 04:19:56 +0300

    lab 3 b unique email on model and db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :DOB, presence: true
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }, uniqueness: true
     validates :phone_number, presence: true
   end
   
