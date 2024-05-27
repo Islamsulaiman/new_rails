e53de86c291fda45fe5e2de3d687e8cd0d87050a
Rehab5 <rehabkosbar@gmail.com>
2024-05-27 09:36:31 +0300

    crud for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,69 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: %i[ show edit update destroy ]
+  
+    # GET /posts
     def index
-        end
+      @posts = Post.all
     end
+  
+    # GET /posts/id
+    def show
+    end
+  
+    # GET /posts/new
+    def new
+      @post = Post.new
+    end
+  
+    # GET /posts/id/edit
+    def edit
+    end
+  
+    # POST /posts
+    def create
+      @post = Post.new(post_params)
+  
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+          format.json { render :show, status: :created, location: @post }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    # PATCH/PUT /posts/id
+    def update
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+          format.json { render :show, status: :ok, location: @post }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    # DELETE /posts/id
+    def destroy
+      @post.destroy!
+  
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    end
+  
+    private
+      
+      def set_post
+        @post = Post.find(params[:id])
+      end
+  
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
+  end
